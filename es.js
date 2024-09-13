92ea64b133a5fb35251f640391f06b88288f71a0
BHARATHI A K <bharathiak2005@gmail.com>
2024-09-13 12:31:21 +0530

    arrow

---
+++ es.js
@@ -0,0 +1,14 @@
+function fun1()
+{
+    for(let i=0;i<=5;i++){
+
+        console.log("FUN1")
+    }
+}
+const fun2=()=>{
+    console.log("FUN2")
+}
+const fun3=()=>console.log("FUN3")
+fun1()
+fun2()
+fun3()
