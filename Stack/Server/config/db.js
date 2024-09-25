95878ace430bda9eb4956f0bb4fa4a88e1312eff
BHARATHI A K <bharathiak2005@gmail.com>
2024-09-25 13:45:44 +0530

    change

--- Stack/Server/config/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGOURL)
-const connection = mongoose.connection;
-connection.on('connected', () => console.log("DB Connected"))
-connection.on('error', () => console.log("DB Error"))
-module.exports = mongoose
